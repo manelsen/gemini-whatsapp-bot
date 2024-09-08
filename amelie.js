bda99d5af802116c29ad5a2e08a7ee606bfbed44
Manel Moura <emanuelmoura@gmail.com>
2024-09-08 14:46:07 -0300

    Configurações de segurança

--- amelie.js
+++ amelie.js
@@ -54,9 +54,21 @@ function createModelWithSystemInstruction(systemInstruction) {
         generationConfig: defaultConfig,
         safetySettings: [
             {
-                category: "HARM_CATEGORY_HARASSMENT",
-                threshold: "BLOCK_MEDIUM_AND_ABOVE"
-            },
+                category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+              },
+              {
+                category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+              },
+              {
+                category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+              },
+              {
+                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE,
+              },
             // Adicione outras configurações de segurança conforme necessário
         ],
         systemInstruction: systemInstruction,
